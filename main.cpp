15934e40492d199c14e6e37efa7ca4e8a4ad3da2
Edwin Kofler <edwin@kofler.dev>
2024-09-19 00:48:57 -0700

    Add more questions to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,20 @@
 int main() {
         std::cout << "This is a simple program\n";
 
+        std::cout << "What is your name?\n";
+        std::string name;
+        std::cin >> name;
+        std::cout << "Your name is: \"" + name + "\". That is a cool name";
+
+        std::cout << "What is your age?\n";
+        std::string age;
+        std::cin >> age;
+        std::cout << "Your age is: \"" + age + "\"." + age + "? I bet your back hurts...";
+
+        std::cout << "What is your favorite hobby?\n";
+        std::string hobby;
+        std::cin >> hobby;
+        std::cout << "Your favorite hobby is doing \"" + hobby + "\"? That's kind of lame...";
+
         std::cout << "This concludes my cool and epic program. Goodbye!";
 }
